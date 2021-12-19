c2f47d8b54bc7dd6519b9f0d9e8cd482b7d4ad5a
Виктор <oscaruser91@yandex.ru>
2021-12-20 01:08:18 +0300

    Решение 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld secondBean = (HelloWorld) applicationContext.getBean("helloworld");
+
+        boolean compare1 = bean == secondBean;
+        System.out.println(compare1);
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        boolean compare2 = cat1 == cat2;
+        System.out.println(compare2);
+
+
+
     }
 }
